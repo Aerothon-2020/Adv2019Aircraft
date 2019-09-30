2496620cf267ee4fbb2d2392113417b5d3d44d99
mwybo <41376231+mwybo@users.noreply.github.com>
2019-09-30 16:46:38 -0400

    init

---
+++ Controls/Surfaces/HingeMoments.py
@@ -0,0 +1,39 @@
+#from os import environ as _environ; _environ["scalar_off"] = "off"
+
+from Aerothon.ACControls import ACControls
+from Aerothon.ACXFoil import ACXFoil
+from Aerothon import AeroUtil
+from scalar.units import SEC, ARCDEG, LBF, IN, FT, OZF
+from scalar.units import AsUnit
+import pylab as pyl
+import numpy as npy
+from Aircraft_Models.Adv2014Aircraft_AeroCats.MonoWing.Aircraft import Aircraft
+
+
+Execute = True
+
+Vs = npy.linspace(Aircraft.GetV_LO()/(FT/SEC), Aircraft.Vmax()/(FT/SEC), 11)*FT/SEC
+des = npy.linspace(5, 20, 3)*ARCDEG
+alpha2d = 3*ARCDEG
+
+# Use the following to investigate the effects of changing geometry
+#Aircraft.Wing.Aileron.Fc = 0.15
+#Aircraft.Wing.Aileron.Fb = 0.334 #Need adjusted to make aileron 3% from tip
+#Aircraft.Wing.Aileron.Ft = 0.1337
+
+#This are the ratios of the servo arm to the control horn arm. The control arm length is the distance to the hinge.
+# ArmRatio = (Servo Arm)/(Control Arm)
+# Servo Arm Lengths:   Aile: 0.8" , Elev: 0.4" , Rudder: 0.4"
+# Control Arm Lengths: Aile: 1.5", Elev: 1.4", Rudder: 1.5"
+Aileron_ArmRatio = 0.53
+Elevator_ArmRatio = 0.29
+Rudder_ArmRatio = 0.27
+
+Aircraft.Wing.Aileron.PlotHingeMoment(fig=1, RunDir='XFoil/Aileron/', alpha2d=alpha2d, Ht=1, ArmRatio=Aileron_ArmRatio, des=des, Vs=Vs, Execute=Execute)
+pyl.title('Aileron Hinge Moment')
+Aircraft.HTail.Elevator.PlotHingeMoment(fig=2, RunDir='XFoil/Elevator/', alpha2d=alpha2d, Ht=1, ArmRatio=Elevator_ArmRatio, des=des, Vs=Vs, Execute=Execute)
+pyl.title('Elevator Hinge Moment')
+Aircraft.VTail.Rudder.PlotHingeMoment(fig=3, RunDir='XFoil/Rudder/', alpha2d=alpha2d, Ht=0.5, ArmRatio=Rudder_ArmRatio, des=des, Vs=Vs, Execute=Execute)
+pyl.title('Rudder Hinge Moment')
+
+pyl.show()
